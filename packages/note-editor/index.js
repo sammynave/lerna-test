7f83a381bd79c3283ce5a43390c9a45f269e6342
Sammy Nave <sammy.nave@gmail.com>
2018-10-31 15:33:02 -0400

    rename

--- packages/note-editor/index.js
+++ packages/note-editor/index.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
-	(global.NoteEditor = factory());
+	(global.LovepopTestNoteEditor = factory());
 }(this, (function () { 'use strict';
 
 	function noop() {}
